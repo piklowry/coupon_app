ba585828be4d28d3ca372339d2a5a0ff54e97680
Anna Cho <annacho13@gmail.com>
2014-05-01 17:30:34 -0400

    fixed create coupon form

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -61,6 +61,6 @@ class CouponsController < ApplicationController
   private
 
   def coupon_params
-    params.require(:coupon).permit(:title, :description, :company_name, :sale_order, :coupon_pic)
+    params.require(:coupon).permit!
   end 
 end
