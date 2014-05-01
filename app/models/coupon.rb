b4642085c76a250578771a758dc8d74d0d11a894
patrick <piklowry05@gmail.com>
2014-05-01 08:12:12 -0400

    fourth commit

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -8,5 +8,15 @@ class Coupon < ActiveRecord::Base
 	has_many :categories, :through => :users
 	
 	mount_uploader :coupon_pic, CouponPicUploader
+
+
+def self.search(params)
+		name = params[:name]
+	  search = Coupon.all
+		if name.present?
+    	search = search.where('coupons.title LIKE ?', "%#{name}%")
+		end
+		search
+	end
 end
 
